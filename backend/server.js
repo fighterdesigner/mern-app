6fa5c8c31c9a745fc33afbbf34508460f9cf4c0e
fighterdesigner <designsleader@gmail.com>
2022-07-04 13:25:59 -0700

    setup frontend server

--- backend/server.js
+++ backend/server.js
@@ -16,14 +16,5 @@ connectToDb()
 app.use("/api/todolist", require('./routes/todoListRoute'))
 app.use("/api/user", require('./routes/userRoute'))
 
-// check if we are in production
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/build")))
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html"))
-    })
-}
-
 app.use(errorHandler)
 app.listen(port, () => console.log(`server is running on port ${port}`))
